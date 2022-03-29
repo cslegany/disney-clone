f15f97e08877645935df49335b23d09cdeb24cf5
Csaba Legány <cslegany@synerinsoft.com>
2022-03-29 09:14:25 +0200

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        home: "url('/images/background.png')",
+      }),
+    },
+    fontFamily: {
+      body: ["Montserrat", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")]
+}
